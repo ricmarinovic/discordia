376e1f6d4033aed5203149afedb4237da4decdfd
Siderlan Santos <siderlan.santos@gmail.com>
2017-10-06 15:27:10 -0300

    Bump version
    Update the app version to 0.0.2.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Discordia.Mixfile do
 
   def project do
     [app: :discordia,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
