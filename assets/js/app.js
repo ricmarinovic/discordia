0266f434e0d3b62159d4b710c97ea6faaa039a6c
Ricardo Marinovic <marinovic.ricardo@gmail.com>
2019-06-05 20:44:32 -0300

    Prepare app for update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,17 @@
+// We need to import the CSS so that webpack will load it.
+// The MiniCssExtractPlugin is used to separate it out into
+// its own CSS file.
+import css from "../css/app.css"
+
+// webpack automatically bundles all modules in your
+// entry points. Those entry points can be configured
+// in "webpack.config.js".
+//
+// Import dependencies
+//
 import "phoenix_html"
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-
-import rootReducer from './reducers'
-import Index from './components/index'
-
-let store = createStore(rootReducer)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Index />
-  </Provider>
-  , document.getElementById('root')
-)
+// Import local files
+//
+// Local files can be imported directly using relative paths, for example:
+// import socket from "./socket"
--- config/test.exs
+++ config/test.exs
@@ -2,19 +2,9 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :discordia, Discordia.Web.Endpoint,
-  http: [port: 4001],
+config :discordia, DiscordiaWeb.Endpoint,
+  http: [port: 4002],
   server: false
 
 # Print only warnings and errors during test
 config :logger, level: :warn
-
-import_config "test.secret.exs"
-# # Configure your database
-# config :discordia, Discordia.Repo,
-#   adapter: Ecto.Adapters.Postgres,
-#   username: "postgres",
-#   password: "postgres",
-#   database: "discordia_test",
-#   hostname: "localhost",
-#   pool: Ecto.Adapters.SQL.Sandbox
